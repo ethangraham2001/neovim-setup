102122b9091241221fa6a767d775f1d4983420c0
Ethan <ethan.graham@epfl.ch>
2024-05-04 14:38:42 +0200

    added function for quickly switching colors with <leader>nc

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -17,4 +17,18 @@ function ColorScheme(scheme_name)
     vim.cmd('colorscheme ' .. scheme_name)
 end
 
+local color_scheme_names = {'gruvbox', 'spaceduck', 'newpaper', 'monokai-pro'}
+local curr_scheme_idx = 1
+
+function NextColorScheme()
+    curr_scheme_idx = curr_scheme_idx + 1
+    if curr_scheme_idx > #color_scheme_names then 
+        curr_scheme_idx = 1
+    end
+
+    local nextScheme = color_scheme_names[curr_scheme_idx]
+    ColorScheme(nextScheme)
+end
+vim.keymap.set("n", "<leader>nc", ":lua NextColorScheme()<cr>")
+
 TransparentBackground(true)
