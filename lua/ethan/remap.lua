52ce0c7c8dafbb35accf474283865b34821254f1
Ethan Graham <ethan.graham@orbitalize.com>
2024-06-25 16:09:34 +0200

    added remap for printing file directory

--- lua/ethan/remap.lua
+++ lua/ethan/remap.lua
@@ -3,4 +3,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", opts);
 vim.keymap.set("n", "<leader>d", ':lua vim.diagnostic.open_float(0, {scope="line"})<cr>')
+vim.keymap.set("n", "<leader>pwd", ":lua print(vim.fn.expand(\"%:p:h\"))<CR>", opts)
 
